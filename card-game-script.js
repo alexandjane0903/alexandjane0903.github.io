aab877c5d4b15946473f95f0e4ad08f5b68873d3
Alex <69563006+alexandjane0903@users.noreply.github.com>
2020-08-14 11:37:46 +0800

    Add files via upload

---
+++ card-game-script.js
@@ -0,0 +1,104 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+let finishedCardPairs = 0;
+
+function flipCard() {
+  // 剛剛沒配對成功的話，就把牌蓋起來
+  if (lockBoard) return;
+
+  // 避免翻同一張牌當做第二張
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this; // this => the clicked card
+    return;
+  }
+
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  // 如果牌組配對成功 => isMatch
+  // 就不可以再點擊那組牌 => disableCards()
+  // 配對錯誤就把該牌組蓋起來 => unflipCards()
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+
+  checkFinished();
+}
+
+function disableCards() {
+  finishedCardPairs += 1;
+  // 移除監聽事件，釋放記憶體
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  // 把牌蓋起來
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+  }, 1200);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function checkFinished() {
+  if(finishedCardPairs == ((cards.length/2))){
+    setTimeout(() => {
+      window.alert("通關暗號是：「******」");
+    }, 800);
+  }
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
